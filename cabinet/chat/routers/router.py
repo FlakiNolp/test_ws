ab51115e4c1e9473ddee96d7916c8a425c34c016
Maxim <mvosetrov@edu.hse.ru>
2023-10-07 00:16:00 +0500

    json version

--- cabinet/chat/routers/router.py
+++ cabinet/chat/routers/router.py
@@ -15,12 +15,12 @@ class ConnectionManager:
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+    async def send_personal_message(self, message: dict, websocket: WebSocket):
+        await websocket.send_json(message)
 
     async def broadcast(self, message: str):
         for connection in self.active_connections:
-            await connection.send_text(message)
+            await connection.send_json(message)
 
 
 manager = ConnectionManager()
@@ -45,9 +45,8 @@ async def websocket_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
     try:
         while True:
-            data = await websocket.receive_text()
-            data = data.split()
-            await manager.broadcast(f"{data[0]}: {data[1]}")
+            data = await websocket.receive_json()
+            await manager.broadcast(data)
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         await manager.broadcast(f"Client left the chat")
